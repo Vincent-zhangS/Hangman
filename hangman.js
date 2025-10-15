2a0bf5e949ef07197cfc5faa42d0e2789ad1bd73
Vincent-zhangS <vzhang2022@gmail.com>
2025-10-15 10:42:59 -0400

    Hangman.js

---
+++ hangman.js
@@ -0,0 +1,4 @@
+var POSSIBLE_WORDS = ["obdurate","verisimiltude","defenstrate","obsequious","dissonant","today","idempotent"];
+
+var randomIndex = Math.random()*POSSIBLE_WORDS.length
+var word = POSSIBLE_WORDS[randomIndex]
