e5ddca1cd92669df8f77d0c18dd8ebcb4690b436
Vincent-zhangS <vzhang2022@gmail.com>
2025-10-20 10:06:52 -0400

    commit

--- hangman.js
+++ hangman.js
@@ -7,9 +7,8 @@ function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length)
     word = POSSIBLE_WORDS(randomIndex);
     var clueString ="";
-    for(var i =0;i<word.length;i++)
-    {
-        clueString+= "_";
+    for(var i =0;i<word.length;i++){
+        clueString+= "_ ";
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
