9703aa1529ad0d0c42396bcb4b407efb36433cb0
Vincent-zhangS <vzhang2022@gmail.com>
2025-10-22 10:17:32 -0400

    got the _ to work

--- hangman.js
+++ hangman.js
@@ -1,43 +1,50 @@
-var POSSIBLE_WORDS = ["obdurate","verisimiltude","defenstrate","obsequious","dissonant","today","idempotent"];
+var POSSIBLE_WORDS = [
+  "obdurate",
+  "verisimilitude",
+  "defenstrate",
+  "obsequious",
+  "dissonate",
+  "toady",
+  "idempotent",
+];
+
 var word = "";
 var guesses = "";
-var Max_Guesses = 6;
-var guess_count = 0
-function newGame(){
-    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length)
-    word = POSSIBLE_WORDS(randomIndex);
-    guesses = "";
-    guess_count = Max_Guesses;
-    updatePage();
-
-    }
-    
-function guessLetter(){
-    var input = document.getElementById("guess");
-    var letter = input.value;
-    if (word.indexOf(letter) < 0){
-        guess_count --;
-    }
-    guesses += letter;
-    updatePage();
-    input.value = "";
+var MAX_GUESSES = 6;
+var guess_count = 0;
+function newGame() {
+  var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+  word = POSSIBLE_WORDS[randomIndex];
+  guesses = "";
+  guess_count = MAX_GUESSES;
+  updatepage();
 }
-function updatePage(){
-    var clueString = "";
-    for(var i = 0; i < word.length; i++){
-        var currentLetter = word.charAt(i)
-        if (guessLetter.indexOf(currentLetter) >= 0){// You Guessed it
-        clueString += currentLetter + " ";
-        }
-        else
-            clueString+= "_ ";
-    }
-    var clue = document.getElementById("clue");
-    clue.innerHTML = clueString;
 
-    var guessArea = document.getElementById("guesses");
-    guessArea.innerHTML = "Guessed Letters: " + guesses;
+function guessLetter() {
+  var input = document.getElementById("guess");
+  var letter = input.value;
+  if (word.indexOf(letter) < 0) {
+    guess_count--;
+  }
+  guesses += letter;
+  updatepage();
+}
 
-    var image = document.getElementById("hangmanImage");
-    image.src = "images/hangman" + guess_count + ".gif";
+function updatepage() {
+  var clueString = "";
+  for (var i = 0; i < word.length; i++) {
+    var currentLetter = word.charAt(i);
+    if (guesses.indexOf(currentLetter) >= 0) {
+      clueString += currentLetter + " ";
+    } 
+    else clueString += "_ ";
+  }
+  var clue = document.getElementById("clue");
+  clue.innerHTML = clueString;
+
+  var guessArea = document.getElementById("guesses");
+  guessArea.innerHTML = "Guessed Letter " + guesses;
+
+  var image = document.getElementById("hangmanImage");
+  image.src = "images/hangman" + guess_count + ".gif";
 }
