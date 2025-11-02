dfcf612ce670145a9d94d4f5e02941f9d20f8898
Vincent-zhangS <vzhang2022@gmail.com>
2025-11-02 01:29:45 -0400

    add a check for lower or uppercase, a clue button, and win/lose

--- hangman.js
+++ hangman.js
@@ -1,11 +1,9 @@
 var POSSIBLE_WORDS = [
-  "obdurate",
-  "verisimilitude",
-  "defenstrate",
-  "obsequious",
-  "dissonate",
-  "toady",
-  "idempotent",
+  "Easter",
+  "Halloween",
+  "Valentine",
+  "Christmas",
+  "Thanksgiving",
 ];
 
 var word = "";
@@ -22,8 +20,12 @@ function newGame() {
 
 function guessLetter() {
   var input = document.getElementById("guess");
-  var letter = input.value;
-  if (word.indexOf(letter) < 0) {
+  
+  //Makes it so it doesnt matter if its upper or lower case
+  var letter = input.value.toLowerCase(); 
+  var lowerWord = word.toLowerCase();    
+
+  if (lowerWord.indexOf(letter) < 0) {
     guess_count--;
   }
   guesses += letter;
@@ -34,17 +36,65 @@ function updatepage() {
   var clueString = "";
   for (var i = 0; i < word.length; i++) {
     var currentLetter = word.charAt(i);
-    if (guesses.indexOf(currentLetter) >= 0) {
+    
+    // This compares lowercase words
+    if (guesses.toLowerCase().indexOf(currentLetter.toLowerCase()) >= 0) {
       clueString += currentLetter + " ";
-    } 
-    else clueString += "_ ";
+    } else {
+      clueString += "_ ";
+    }
   }
   var clue = document.getElementById("clue");
   clue.innerHTML = clueString;
 
   var guessArea = document.getElementById("guesses");
-  guessArea.innerHTML = "Guessed Letter " + guesses;
+  guessArea.innerHTML = "Guessed Letters: " + guesses;
 
   var image = document.getElementById("hangmanImage");
   image.src = "images/hangman" + guess_count + ".gif";
 }
+
+// Gives a clue
+function giveClue() {
+  var clueText = "";
+  var lowerWord = word.toLowerCase();
+
+  if (lowerWord == "easter") {
+    clueText = "A spring holiday involving eggs and a bunny.";
+  } else if (lowerWord == "halloween") {
+    clueText = "A spooky day with costumes and candy.";
+  } else if (lowerWord == "valentine") {
+    clueText = "A day involving hearts, chocolate, and love.";
+  } else if (lowerWord == "christmas") {
+    clueText = "A winter holiday where you give and receive gifts.";
+  } else if (lowerWord == "thanksgiving") {
+    clueText = "A day of turkey and being thankful.";
+  } else {
+    clueText = "start a new game first";
+  }
+
+  console.log("Clue: " + clueText);
+}
+
+function wonGame() {
+  var clue = document.getElementById("clue").innerHTML;
+
+  // if no more underscores then you win
+  if (clue.indexOf("_") === -1) {
+    return true;
+  }
+
+  return false;
+}
+
+//checks for if you win or not
+document.addEventListener("keyup", function () {
+  if (wonGame()) {
+    document.getElementById("usedletters").style.color = "green";
+    document.getElementById("usedletters").innerHTML = "Congrats";
+  }
+  else if (guess_count === 0) {
+    document.getElementById("usedletters").style.color = "red";
+    document.getElementById("usedletters").innerHTML = "Whomp Whomp :/";
+  }
+});
